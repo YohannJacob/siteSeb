3c45c5b35ee82e9ada0338eb529d7c42fe285219
YohannJacob <yohann@yohannjacob.fr>
2019-08-05 09:40:58 +0200

    Initial commit

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    // Route en front
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+
+
+    /**
+     * @Route("/mesAlbums", name="mesAlbums")
+     */
+    public function mesAlbums()
+    {
+        return $this->render('blog/mesalbums.html.twig');
+    }
+
+    /**
+     * @Route("/album/12", name="album")
+     */
+    public function album()
+    {
+        return $this->render('blog/album.html.twig');
+    }
+
+    /**
+     * @Route("/aPropos", name="aPropos")
+     */
+    public function aPropos()
+    {
+        return $this->render('blog/apropos.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact()
+    {
+        return $this->render('blog/contact.html.twig');
+    }
+
+
+    // route en back
+
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function admin()
+    {
+        return $this->render('blog/admin.html.twig');
+    }
+
+    /**
+     * @Route("/createAlbum", name="createAlbum")
+     */
+    public function createAlbum()
+    {
+        return $this->render('blog/createAlbum.html.twig');
+    }
+
+    /**
+     * @Route("/updateAlbum", name="updateAlbum")
+     */
+    public function updateAlbum()
+    {
+        return $this->render('blog/updateAlbum.html.twig');
+    }
+
+    /**
+     * @Route("/createNews", name="createNews")
+     */
+    public function createNews()
+    {
+        return $this->render('blog/createNews.html.twig');
+    }
+
+    /**
+     * @Route("/updateNews", name="updateNews")
+     */
+    public function updateNews()
+    {
+        return $this->render('blog/updateNews.html.twig');
+    }
+}
